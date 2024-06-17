60e274504426fbcb1245de6a650d76acb34941c0
David Hong <davidbor@net.chalmers.se>
2024-06-17 13:34:40 +0200

    #15 Improve HTTP messages  This commit will enhance the HTTP messages and translate them to Korean  as the target audience are Korean people.

--- src/controllers/programs-controller.js
+++ src/controllers/programs-controller.js
@@ -16,13 +16,15 @@ router.get("/", async (req, res) => {
         const programs = await Programs.find();
 
         if (programs.length === 0) {
-            return res.status(404).send("No programs found!!");
+            // Translation: This is not a registered program
+            return res.status(404).send("시스템에 등록된 프로그램이 없습니다.");
         }
 
-        return res.send(programs);
+        return res.status(200).send(programs);
     } catch (err) {
         console.log(err);
-        return res.status(500).send("An internal server error has occured.");
+        // Translation: An internal server error has occured
+        return res.status(500).send("시스템상 에러가 발생하였습니다.");
 
     }
 });
@@ -69,7 +71,8 @@ router.get("/byPrerequisites", async (req, res) => {
 
         // Returns invalid prerequisites as the error message.
         if (invalidPrerequisites.length > 0) {
-            return res.status(400).json({ error: "Invalid prerequisites specified: " + invalidPrerequisites.join(", ") });
+            // Translation: Invalid prerequisite(s) were requested
+            return res.status(400).json({ error: "존재하지 않는 자격요건: " + invalidPrerequisites.join(", ") });
         }
 
         // Aggregate to find all programs that meet the prerequisites
@@ -99,7 +102,8 @@ router.get("/byPrerequisites", async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "An internal server error has occurred" });
+        // Translation: An internal server erro has occured
+        return res.status(500).json({ error: "시스템상 에러가 발생하였습니다." });
     }
 });
 
@@ -116,7 +120,8 @@ router.get("/byUniversity", async (req, res) => {
         const requestedUniversity = req.query.universityName;
 
         if (!requestedUniversity) {
-            return res.status(400).json({ error: "No university provided!!" });
+            // Translation: No university provided
+            return res.status(400).json({ error: "대학교 정보가 입력되지 않았습니다." });
         }
 
         console.log(requestedUniversity);
@@ -124,14 +129,16 @@ router.get("/byUniversity", async (req, res) => {
         const programsWithUniversity = await Programs.find({ universityName: requestedUniversity });
 
         if (programsWithUniversity.length === 0) {
-            return res.status(400).json({ error: "University not registered in the system" });
+            // Translation: University not registered in the system
+            return res.status(400).json({ error: "시스템에 등록되지 않은 대학교입니다." });
         }
 
         return res.status(200).json({ programs: programsWithUniversity });
 
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "An internal server error has occurred" });
+        // Translation: An internal server error has occured
+        return res.status(500).json({ error: "시스템상 에러가 발생하였습니다." });
     }
 });
 
@@ -147,12 +154,14 @@ router.get("/:id", async (req, res) => {
          program = await Programs.findOne({_id: req.params.id });
 
         if (program == null) {
-            return res.status(404).send("Program not found");
+            // Translation: Program not found
+            return res.status(404).send("시스템에 등록되지 않은 프로그램입니다.");
         }
     return res.send(program);
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "An internal servver error has occured" });
+        // Translation: An internal server error has occured
+        return res.status(500).json({ error: "시스템상 에러가 발생하였습니다." });
     }
 });
 
@@ -177,18 +186,21 @@ router.post("/", async (req, res) => {
         const tuitionFee = req.body.programTuition;
 
         if (!name || !programCode || !universityName || !prerequisite || !tuitionFee) {
-            return res.status(400).json({ error: "To create a new program, name, program code, university name, and prerequisites are required." });
+            // Translation: To create a new program the following information is required: name, program code, university name, prerequisites, and tuitionFee
+            return res.status(400).json({ error: "새로운 프로그램을 등록하기 위해서 다음 정보가 필요합니다: 이름, 학과코드, 자격요건, 학비" });
         }
 
         const invalidPrerequisites = prerequisite.filter(p => !["Math3B", "Math4", "Math5", "Physics1A", "Physics2", "Chemistry1", "Chemistry2", "Biology1", "Biology2", "Science2", "Civics1B", "History1B", "SpecialRequirement"].includes(p));
         if (invalidPrerequisites.length > 0) {
-            return res.status(400).json({ error: "Invalid prerequisites: " + invalidPrerequisites.join(", ") });
+            // Translation: Invalid prerquisite(s)
+            return res.status(400).json({ error: "존재하지 않는 자격요건: " + invalidPrerequisites.join(", ") });
         }
 
         const existingProgram = await Programs.findOne({ name: name });
 
         if (existingProgram) {
-            return res.status(409).json({ error: "The following program is already registered in the system." });
+            // Translation: The following program is already registered in the system
+            return res.status(409).json({ error: "다음 프로그램은 이미 시스템에 등록되어 있습니다." });
         }
 
         const newProgram = new Programs ({
@@ -208,10 +220,12 @@ router.post("/", async (req, res) => {
             { new: true }
         );
 
-        return res.status(201).json({ message: "New Program Saved", program: newProgram });
+        // Translation: New program saved
+        return res.status(201).json({ message: "새로운 프로그램이 등록되었습니다.", program: newProgram });
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "An internal server error occured" });
+        // Translation: An internal server error has occured
+        return res.status(500).json({ error: "시스템상 오류가 발생하였습니다." });
     }
 });
 
@@ -228,7 +242,8 @@ router.patch("/:id", async (req, res) => {
         const existingProgram = await Programs.findById(req.params.id);
 
         if (!existingProgram) {
-            res.status(404).json({ error: "Program with the following id does not exist" });
+            // Translation: Program with the following ID does not exist
+            res.status(404).json({ error: "다음 ID로 등록된 프로그램이 존재하지 않습니다." });
         }
         
         const oldInfo = existingProgram.toObject();
@@ -245,7 +260,8 @@ router.patch("/:id", async (req, res) => {
         return res.status(200).send(await Programs.findById(req.params.id));
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "An internal server error has occured"});
+        // Translation: An internal server error has occured
+        return res.status(500).json({ error: "시스템상 오류가 발샹하였습니다."});
     }
 
 })
@@ -263,15 +279,18 @@ router.delete("/:id", async (req, res) => {
         const existingProgram = await Programs.findOne({ _id: req.params.id });
 
         if (!existingProgram) {
-            return res.status(404).json({ error: "Program with the following id does not exist" });
+            // Translation: Program with the following ID does not exist
+            return res.status(404).json({ error: "다음 ID로 등록된 프로그램이 존재하지 않습니다." });
         }
 
         await Programs.deleteOne({ _id: req.params.id });
-        return res.status(200).json({ message: "Deleted Successfully!!"});
+        // Translation: Deleted successfully
+        return res.status(200).json({ message: "성공적으로 삭제하였습니다."});
 
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "An internal server error occured" });
+        // Translation: An internal server error has occured
+        return res.status(500).json({ error: "시스템상 오류가 발생하였습니다." });
     }
 });
 
