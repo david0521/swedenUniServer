cf542e9f64bcbff2a127b3f5136776276b65d6ce
David Hong <davidbor@net.chalmers.se>
2024-07-10 11:49:21 +0200

    #60 Statistics collection  This commit will add a collection to store the statistics of program admission average information.

---
+++ src/schemas/statistics.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const user = require('../schemas/user');
+const Schema = mongoose.Schema;
+
+const statisticBaseSchema = new Schema({
+    dateOfCreation: {
+        type: Date,
+    }
+});
+
+const minMeritStats = new Schema({
+    programName: {
+        type: String,
+        required: true
+    },
+    round: {
+        type: String,
+        enum: ['round1', 'round2'],
+        required: true
+    },
+    selectionGroup: {
+        type: String,
+        enum: ['B1', 'B2', 'B1AV', 'B1BF', 'B2AV', 'B2BF'],
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    }
+})
+
+const programLikeStats = new Schema({
+    programName: {
+        type: String,
+        required: true,
+    },
+    numOfLikes: {
+        type: Number,
+        required: true
+    },
+    user: [{
+        type: Schema.Types.ObjectId,
+        required: true
+    }]
+})
+
+const universityLikeStats = new Schema({
+    universityName: {
+        type: String,
+        required: true
+    },
+    numOfLikes: {
+        type: Number,
+        required: true
+    },
+    user: [{
+        type: Schema.Types.ObjectId,
+        required: true
+    }]
+})
+
+const StatisticBase = mongoose.model('StatisticBase', statisticBaseSchema);
+
+const MinMeritStats = StatisticBase.discriminator('MinMeritStats', minMeritStats);
+const ProgramLikeStats = StatisticBase.discriminator('ProgramLikeStats', programLikeStats);
+const UniversityLikeStats = StatisticBase.discriminator('UniversityLikeStats', universityLikeStats);
+
+module.exports = {
+    StatisticBase,
+    MinMeritStats,
+    ProgramLikeStats,
+    UniversityLikeStats
+}
--- src/services/statistics.service.js
+++ src/services/statistics.service.js
@@ -1,23 +1,47 @@
 const Records = require("../schemas/records.js");
+const { MinMeritStats } = require("../schemas/statistics.js");
 
-async function selectionGroupAvg(programName, selectionGroup) {
+async function selectionGroupAvg(programName, selectionGroup, round) {
     try {
         let avgScore = 0;
-        const records = await Records.find({ programName: programName, selectionGroup: selectionGroup });
+        const records = await Records.find({ programName: programName, round: round, selectionGroup: selectionGroup });
         const numOfRecords = records.length;
 
         if (numOfRecords.length === 0) {
-            return 0;
+            avgScore = 0;
         }
 
         else {
             for (let record of records) {
                 avgScore += record.minScore;
             }
-            return avgScore / numOfRecords;
+            avgScore = avgScore / numOfRecords;
         }
+
+        const minScore = await MinMeritStats.findOne({ programName: programName, round: round, selectionGroup: selectionGroup });
+        
+        if (minScore) {
+            await minScore.deleteOne();
+        }
+
+        const newMinScore = new MinMeritStats ({
+            dateOfCreation: new Date(),
+            programName: programName,
+            round: round,
+            selectionGroup: selectionGroup,
+            score: avgScore
+        })
+
+        await newMinScore.save();
+
+
+
     } catch (error) {
         console.error('Error fetching records:', error);
         throw error;
     }
+}
+
+module.exports = {
+    selectionGroupAvg
 }
